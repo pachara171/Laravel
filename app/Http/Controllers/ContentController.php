fc0c4393fff326e789826b7578c2190f232372bf
pachara171 <pachara1045@gmail.com>
2023-04-16 22:38:39 +0700

    Create CRUD

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -1,12 +1,54 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Content;
+use App\Http\Requests\ContentRequest;
 
 use Illuminate\Http\Request;
 
 class ContentController extends Controller
 {
     public function index() {
-        return view('content.index');
+        $contents = Content::all();
+
+        return view('content.index', compact('contents'));
     }
+
+    public function create() {
+        $content = new Content;
+        return view('content.form', compact('content'));
+    }
+    
+    public function store(ContentRequest $request) {
+        $content = new Content();
+        $this->save($content, $request);
+
+        return redirect('/content');
+    }
+
+    public function edit($id) {
+        $content = Content::findOrFail($id);
+        return view('content.form', compact('content'));
+    }
+
+    public function update (ContentRequest $request, $id) {
+        $content = Content::findOrFail($id);
+        $this->save($content, $request);
+        
+        return redirect('/content');
+    }
+    
+    public function destroy($id) {
+        Content::destroy($id);
+    }
+
+    private function save($data, $value) {
+        $data->topic = $value->topic;
+        $data->description = $value->description;
+        $data->tags = $value->tags;
+        $data->user_id = 1;
+        $data->save();
+    }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,10 @@ use App\Http\Controllers\ContentController;
 */
 
 Route::get('/', [ContentController::class, 'index']);
+Route::get('/content', [ContentController::class, 'index']);
+Route::get('/content/create', [ContentController::class, 'create']);
+Route::get('/content/{id}/edit', [ContentController::class, 'edit']);
+
+Route::post('/content', [ContentController::class, 'store']);
+Route::put('/content/{id}', [ContentController::class, 'update']);
+Route::delete('/content/{id}', [ContentController::class, 'destroy']);
